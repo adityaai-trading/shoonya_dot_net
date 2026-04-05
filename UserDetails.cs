02b0a525795544e3367ee428fb5e8c1d1d149577
AdityaAI <support@adityaai.in>
2026-04-05 17:51:18 +0530

    UserDetails

--- UserDetails.cs
+++ UserDetails.cs
@@ -6,10 +6,10 @@ string jsonPayload = $"{{\"uid\":\"{uid}\"}}";
 // 3. Wrap it in the jData parameter and use application/x-www-form-urlencoded
 var content = new StringContent($"jData={jsonPayload}", Encoding.UTF8, "application/x-www-form-urlencoded");
 
-const string ORDER_URL = "https://trade.shoonya.com/NorenWClientAPI/UserDetails";
+const string UserDetails_URL = "https://trade.shoonya.com/NorenWClientAPI/UserDetails";
 
 _httpClient.DefaultRequestHeaders.Authorization =
     new AuthenticationHeaderValue("Bearer", token);
-var response = await _httpClient.PostAsync(ORDER_URL, content);
+var response = await _httpClient.PostAsync(UserDetails_URL, content);
 string result = await response.Content.ReadAsStringAsync();
 Console.WriteLine(result);
